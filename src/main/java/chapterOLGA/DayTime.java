2d44f42e486712ccbd851fec51c695c8f655895c
Olga Gogoleva <olga.gogoleva.can@gmail.com>
2022-11-08 15:35:35 -0800

    Arrays

---
+++ src/main/java/chapterOLGA/DayTime.java
@@ -0,0 +1,24 @@
+package chapterOLGA;
+
+import java.util.Scanner;
+
+public class DayTime {
+    public static void main(String[] args) {
+        String[] dayTime = {"Morning", "Afternoon", "Evening", "Night"};
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter time in a 24h format");
+        int input = scanner.nextInt();
+        scanner.close();
+
+        if (input >= 6 && input <= 12) {
+            System.out.println("This is " + dayTime[0]);
+        } else if (input > 12 && input <= 18) {
+            System.out.println("It is " + dayTime[1]);
+        } else if (input > 18 && input <= 23) {
+            System.out.println("It is " + dayTime[2]);
+        } else {
+            System.out.println("It is " + dayTime[3]);
+        }
+    }
+}
