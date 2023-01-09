7074c213176aeb021da965802a0ca5a5e6036da6
OlgaGogo <olga.gogoleva.can@gmail.com>
2023-01-09 13:04:04 -0800

    Palindrome solutions

---
+++ src/main/java/extra_practice/Palindrome.java
@@ -0,0 +1,34 @@
+package extra_practice;
+
+public class Palindrome {
+    public static void main(String[] args){
+        //check if a string is a palindrome
+        Palindrome palindrome = new Palindrome();
+        String[] arr = {"ab", "ca", "ada", "bf", "aca"};
+        String res = palindrome.firstPalindrome(arr);
+        System.out.println(res);
+
+    }
+
+    public boolean isPalindrome(String s){
+        for(int i = 0; i < s.length()/2; i++){
+            if(s.charAt(i) != s.charAt(s.length() - 1 - i))
+                return false;
+        }
+        return true;
+    }
+    public String firstPalindrome(String[] words){
+        for (String w: words){
+            if(isPalindrome(w)){
+                return w;
+            }
+        }
+        return "";
+    }
+
+    private boolean isPalindrome2(String s){
+        StringBuilder sb = new StringBuilder(s);
+        String reverse = sb.reverse().toString();
+        return s.equals(reverse);
+    }
+}
