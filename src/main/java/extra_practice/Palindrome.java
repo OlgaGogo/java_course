70667949ec44c97f2aa122631300f804e18ffbe2
OlgaGogo <olga.gogoleva.can@gmail.com>
2023-01-09 16:52:48 -0800

    Palindrome solutions

--- src/main/java/extra_practice/Palindrome.java
+++ src/main/java/extra_practice/Palindrome.java
@@ -9,23 +9,33 @@ public class Palindrome {
         System.out.println(res);
 
     }
-    //using for-loop (more effective)
-    public boolean isPalindrome(String s){
-        for(int i = 0; i < s.length()/2; i++){
-            if(s.charAt(i) != s.charAt(s.length() - 1 - i))
-                return false;
-        }
-        return true;
-    }
+
     public String firstPalindrome(String[] words){
         for (String w: words){
-            if(isPalindrome(w)){
+            if(isPalindrome3(w))
                 return w;
-            }
         }
         return "";
     }
 
+    //using for-loop (more effective)
+    public boolean isPalindrome(String s){
+        for(int i = 0; i < s.length()/2; i++)
+            if(s.charAt(i) != s.charAt(s.length() - 1 - i))
+                return false;
+        return true;
+    }
+
+    //using while-loop (more effective)
+    private boolean isPalindrome3(String s){
+        int i=0, j=s.length()-1;
+        while (i<j){
+            if(s.charAt(i++) != s.charAt(j--))
+                return false;
+        }
+        return true;
+
+    }
     //using StringBuilder (less effective)
     private boolean isPalindrome2(String s){
         StringBuilder sb = new StringBuilder(s);
